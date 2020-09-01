72183eb86bb16842b2e0c04ebd0f53da60dc30e6
Linux User <dsleung@dsleung-qemu.my.domain>
2020-09-01 11:28:23 +0800

    Allow database service to stabilize by adding 10 second delay

--- launch.sh
+++ launch.sh
@@ -2,6 +2,8 @@
 
 ./cleanup.sh
 sudo docker container run --rm -d -e MYSQL_ROOT_PASSWORD=root-passwd -e MYSQL_DATABASE=voters -e MYSQL_USER=voters -e MYSQL_PASSWORD=voters-pw --name linux-rocks-database mariadb
+echo 'Waiting 10 seconds for database service to stabilize ...'
+sleep 10
 sudo docker image build -t linux-rocks .
 sudo docker container run --rm -d -p 80:80 --name linux-rocks-webserver linux-rocks
 DATABASE_IP=$(sudo docker container exec linux-rocks-database hostname -i)
